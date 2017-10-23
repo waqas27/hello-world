39de978625517e4eebb274a506bd62aae4cd471f
waqas27 <waqaskhan20@hotmail.co.uk>
2017-10-23 14:43:09 +0100

    Add files via upload

---
+++ task1/task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a Capital letter between A to E: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "A":
+                    Console.WriteLine("1");
+                    break;
+                case "B":
+                    Console.WriteLine("2");
+                    break;
+                case "C":
+                    Console.WriteLine("3");
+                    break;
+                case "D":
+                    Console.WriteLine("4");
+                    break;
+                case "E":
+                    Console.WriteLine("5");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid letter, please try again");
+                    break;
+
+
+            }
+            
+                 
+            }
+        }
+    }
+
