1ebef572d0dc4f239f45543f8b8a5e1731eca43c
waqas27 <waqaskhan20@hotmail.co.uk>
2017-10-23 14:48:03 +0100

    Add files via upload

---
+++ Task2(Palindrome)/Task2(Palindrome)/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_Palindrome_
+{
+    class Program
+    {
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+
+
+                while (!char.IsLetterOrDigit(a))
+                {
+                    min++;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    a = value[min];
+                }
+
+
+                while (!char.IsLetterOrDigit(b))
+                {
+                    max--;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    b = value[max];
+                }
+
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+
+        static void Main()
+        {
+            string[] array =
+            {
+           "I have some cheese",
+           "God saved Eva’s dog",
+           
+           
+        };
+
+            foreach (string value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+            }
+        }
+    }
+}
